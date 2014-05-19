fe061a10bb780211ec03256fa7e298a7252ba168
Steve Tickle <Steve@ZooKoo.com>
2014-05-19 21:36:03 +0100

    - Getting the AVR build up and going.

--- DemoOne/AVR/AppConfiguration.h
+++ DemoOne/AVR/AppConfiguration.h
@@ -21,16 +21,16 @@
 // Define the configuration thru the use of typedefs and where needed c++11 template aliases or
 // a substitute mechanism (inheritance).
 //
-typedef MSP430Output<P1OUT_, P1DIR_, 0x01>                  GreenLEDType;
-typedef MSP430Output<P1OUT_, P1DIR_, 0x02>                  OrangeLEDType;
-typedef MSP430Output<P1OUT_, P1DIR_, 0x04>                  RedLEDType;
-typedef MSP430Output<P1OUT_, P1DIR_, 0x08>                  BlueLEDType;
+typedef AVROutput<0, 0, 0x01>                  GreenLEDType;
+typedef AVROutput<0, 0, 0x02>                  OrangeLEDType;
+typedef AVROutput<0, 0, 0x04>                  RedLEDType;
+typedef AVROutput<0, 0, 0x08>                  BlueLEDType;
 typedef LEDController<  GreenLEDType, 
                         OrangeLEDType, 
                         RedLEDType, 
                         BlueLEDType>                        LEDControllerType;
 class ButtonPressedDelegate;
-typedef MSP430Input<P2IN_, P2DIR_, 0x01, ButtonPressedDelegate> ButtonType;
+typedef AVRInput<0, 0, 0x01, ButtonPressedDelegate> ButtonType;
 
 typedef RocketBloxDemoOne<LEDControllerType>                AppType;
 DELEGATE_TYPE(AppType, ButtonPressed,                       ButtonPressedDelegate);
