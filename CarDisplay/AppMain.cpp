0a21f09cf888a522b6bc47a4d8f480e131c75ea9
U-StevesI7Laptop\steve_000 <steve_000@StevesI7Laptop.(none)>
2014-05-24 11:56:59 +0100

    - More tidying.

--- CarDisplay/AppMain.cpp
+++ CarDisplay/AppMain.cpp
@@ -1,89 +1,42 @@
 
 
 
+#include "Platform.h"
 
 
-
-
-
-/* code for max 7219 from maxim, 
-reduced and optimised for useing more then one 7219 in a row,
-______________________________________
-
- Code History:
- --------------
-
-The orginal code was written for the Wiring board by:
- * Nicholas Zambetti and Dave Mellis /Interaction Design Institute Ivrea /Dec 2004
- * http://www.potemkin.org/uploads/Wiring/MAX7219.txt
-
-First modification by:
- * Marcus Hannerstig/  K3, malmï¿½ hï¿½gskola /2006
- * http://www.xlab.se | http://arduino.berlios.de
-
-This version is by:
- * tomek ness /FH-Potsdam / Feb 2007
- * http://design.fh-potsdam.de/ 
-
- * @acknowledgements: eric f. 
-
------------------------------------
-
-General notes: 
-
-
--if you are only using one max7219, then use the function maxSingle to control
- the little guy ---maxSingle(register (1-8), collum (0-255))
-
--if you are using more then one max7219, and they all should work the same, 
-then use the function maxAll ---maxAll(register (1-8), collum (0-255))
-
--if you are using more than one max7219 and just want to change something
-at one little guy, then use the function maxOne
----maxOne(Max you wane controll (1== the first one), register (1-8), 
-collum (0-255))
-
- During initiation, be sure to send every part to every max7219 and then
- upload it.
-For example, if you have five max7219's, you have to send the scanLimit 5 times
-before you load it-- other wise not every max7219 will get the data. the
-function maxInUse  keeps track of this, just tell it how many max7219 you are
-using.
-*/
-
-#ifdef HWSPI
-#include <SPI.h>
-#endif
-//#include <WProgram.h>
-#include <stdint.h>
-#include <Arduino.h>
-
-int dataIn = 2;
-int load = 3;
-int clock = 4;
-int power=5;
+const int   dataIn  = 2;
+const int   load    = 3;
+const int   clock   = 4;
+const int   power   = 5;
 
 #define maxInUse  (5)    //change this variable to set how many MAX7219's you'll use
-
-int e = 0;           // just a varialble
 
 #define CYCLE_DELAY  delay(0)
 
                      // define max7219 registers
-uint8_t  max7219_reg_noop        = 0x00;
-uint8_t  max7219_reg_digit0      = 0x01;
-uint8_t  max7219_reg_digit1      = 0x02;
-uint8_t  max7219_reg_digit2      = 0x03;
-uint8_t  max7219_reg_digit3      = 0x04;
-uint8_t  max7219_reg_digit4      = 0x05;
-uint8_t  max7219_reg_digit5      = 0x06;
-uint8_t  max7219_reg_digit6      = 0x07;
-uint8_t  max7219_reg_digit7      = 0x08;
-uint8_t  max7219_reg_decodeMode  = 0x09;
-uint8_t  max7219_reg_intensity   = 0x0a;
-uint8_t  max7219_reg_scanLimit   = 0x0b;
-uint8_t  max7219_reg_shutdown    = 0x0c;
-uint8_t  max7219_reg_displayTest = 0x0f;
+
+//
+//
+//
+typedef enum
+{
+    max7219_reg_noop        = 0x00,
+    max7219_reg_digit0      = 0x01,
+    max7219_reg_digit1      = 0x02,
+    max7219_reg_digit2      = 0x03,
+    max7219_reg_digit3      = 0x04,
+    max7219_reg_digit4      = 0x05,
+    max7219_reg_digit5      = 0x06,
+    max7219_reg_digit6      = 0x07,
+    max7219_reg_digit7      = 0x08,
+    max7219_reg_decodeMode  = 0x09,
+    max7219_reg_intensity   = 0x0a,
+    max7219_reg_scanLimit   = 0x0b,
+    max7219_reg_shutdown    = 0x0c,
+    max7219_reg_displayTest = 0x0f,    
+
+} MAX7219Register;
+
 
 
 
@@ -228,7 +181,7 @@ void shiftOutByte (uint8_t  data)
 //
 // maxSingle is the "easy"  function to use for a     //single max7219
 //
-void maxSingle( uint8_t  reg, uint8_t  col) 
+void maxSingle( MAX7219Register  reg, uint8_t  col) 
 {    
     digitalWrite(load, LOW);      // begin     
     shiftOutByte (reg);           // specify register
@@ -243,7 +196,7 @@ void maxSingle( uint8_t  reg, uint8_t  col)
 //
 // initialize  all  MAX7219's in the system
 //
-void maxAll (uint8_t  reg, uint8_t  col) 
+void maxAll (MAX7219Register  reg, uint8_t  col) 
 {    
     int     c = 0;
 
@@ -299,7 +252,7 @@ void drawFrame(uint8_t* frameBuffer)
 
     for(int j=0; j<8; j++)
     {
-        maxAll(j+1, frameBuffer[j]);
+        maxAll( (MAX7219Register)(j+1), frameBuffer[j]);
     }
 }
 
@@ -331,20 +284,20 @@ void setup ()
     digitalWrite(power, HIGH);  
 
     //
-    // initiation of the max 7219
+    // initialisation of the max 7219
     //
-    maxAll(max7219_reg_scanLimit, 0x07);      
-    maxAll(max7219_reg_decodeMode, 0x00);  // using an led matrix (not digits)
-    maxAll(max7219_reg_shutdown, 0x01);    // not in shutdown mode
-    maxAll(max7219_reg_displayTest, 0x00); // no display test
-    maxAll(max7219_reg_intensity, 0x04 & 0x0f);    // the first 0x0f is the value you can set
+    maxAll(max7219_reg_scanLimit,   0x07);      
+    maxAll(max7219_reg_decodeMode,  0x00);          // using an led matrix (not digits)
+    maxAll(max7219_reg_shutdown,    0x01);          // not in shutdown mode
+    maxAll(max7219_reg_displayTest, 0x00);          // no display test
+    maxAll(max7219_reg_intensity,   0x04 & 0x0f);   // the first 0x0f is the value you can set
 
     //
     // empty registers, turn all LEDs off 
     //
-    for (e=1; e<=8; e++) 
+    for (int e=1; e<=8; e++) 
     {    
-        maxAll(e,0);
+        maxAll( (MAX7219Register)e,0);
     }
 }  
 
@@ -355,7 +308,7 @@ void setup ()
 void loop() 
 {
     static  int         i           = 0;
-    static  uint8_t     frameBuffer[8];
+    static  uint8_t     frameBuffer[8*maxInUse];
     static  uint32_t    frameCount  = 0;
     
     //
@@ -367,7 +320,7 @@ void loop()
     // Modify the frame for next time.
     //
     frameCount++;
-    if(frameCount>=8)
+    if(frameCount >= (maxInUse*8))
     {
         frameCount  = 0;
     }
