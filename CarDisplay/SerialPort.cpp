b10cd10149130cc04f402b0e546543c20d56918b
U-StevesI7Laptop\steve_000 <steve_000@StevesI7Laptop.(none)>
2014-06-02 22:53:28 +0100

    - Removed the ifdefs from SerialPort.cpp

--- CarDisplay/SerialPort.cpp
+++ CarDisplay/SerialPort.cpp
@@ -1,27 +1,8 @@
-#define __AVR_LIBC_DEPRECATED_ENABLE__
 
-/*
-  SerialPort.cpp - Hardware serial library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  
-  Modified 23 November 2006 by David A. Mellis
-  Modified 28 September 2010 by Mark Sproul
-  Modified 14 August 2012 by Alarus
-*/
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -29,53 +10,14 @@
 #include <inttypes.h>
 #include "Platform.h"
 #include "wiring_private.h"
-
-
-
-// this next line disables the entire SerialPort.cpp, 
-// this is so I can support Attiny series and any other chip without a uart
-#if defined(UBRRH) || defined(UBRR0H) || defined(UBRR1H) || defined(UBRR2H) || defined(UBRR3H)
-
 #include "SerialPort.h"
 
-/*
- * on ATmega8, the uart and its bits are not numbered, so there is no "TXC0"
- * definition.
- */
-#if !defined(TXC0)
-#if defined(TXC)
-#define TXC0 TXC
-#elif defined(TXC1)
-// Some devices have uart1 but no uart0
-#define TXC0 TXC1
-#else
-#error TXC0 not definable in SerialPort.h
-#endif
-#endif
 
 
-#if defined(USBCON)
-  ring_buffer rx_buffer = { { 0 }, 0, 0};
-  ring_buffer tx_buffer = { { 0 }, 0, 0};
-#endif
-#if defined(UBRRH) || defined(UBRR0H)
-  ring_buffer rx_buffer  =  { { 0 }, 0, 0 };
-  ring_buffer tx_buffer  =  { { 0 }, 0, 0 };
-#endif
-#if defined(UBRR1H)
-  ring_buffer rx_buffer1  =  { { 0 }, 0, 0 };
-  ring_buffer tx_buffer1  =  { { 0 }, 0, 0 };
-#endif
-#if defined(UBRR2H)
-  ring_buffer rx_buffer2  =  { { 0 }, 0, 0 };
-  ring_buffer tx_buffer2  =  { { 0 }, 0, 0 };
-#endif
-#if defined(UBRR3H)
-  ring_buffer rx_buffer3  =  { { 0 }, 0, 0 };
-  ring_buffer tx_buffer3  =  { { 0 }, 0, 0 };
-#endif
 
 
+ring_buffer rx_buffer  =  { { 0 }, 0, 0 };
+ring_buffer tx_buffer  =  { { 0 }, 0, 0 };
 
 
 
@@ -93,111 +35,25 @@ inline void store_char(unsigned char c, ring_buffer *buffer)
   }
 }
 
-#if !defined(USART0_RX_vect) && defined(USART1_RX_vect)
-// do nothing - on the 32u4 the first USART is USART1
-#else
-#if !defined(USART_RX_vect) && !defined(SIG_USART0_RECV) && \
-    !defined(SIG_UART0_RECV) && !defined(USART0_RX_vect) && \
-	!defined(SIG_UART_RECV)
-  #error "Don't know what the Data Received vector is called for the first UART"
-#else
-  void serialEvent() __attribute__((weak));
-  void serialEvent() {}
-  #define serialEvent_implemented
-#if defined(USART_RX_vect)
-  SIGNAL(USART_RX_vect)
-#elif defined(SIG_USART0_RECV)
-  SIGNAL(SIG_USART0_RECV)
-#elif defined(SIG_UART0_RECV)
-  SIGNAL(SIG_UART0_RECV)
-#elif defined(USART0_RX_vect)
-  SIGNAL(USART0_RX_vect)
-#elif defined(SIG_UART_RECV)
-  SIGNAL(SIG_UART_RECV)
-#endif
+
+
+
+
+void serialEvent() __attribute__((weak));
+void serialEvent() {}
+#define serialEvent_implemented
+
+SIGNAL(USART_RX_vect)
   {
-  #if defined(UDR0)
     if (bit_is_clear(UCSR0A, UPE0)) {
       unsigned char c = UDR0;
       store_char(c, &rx_buffer);
     } else {
       unsigned char c = UDR0;
     };
-  #elif defined(UDR)
-    if (bit_is_clear(UCSRA, PE)) {
-      unsigned char c = UDR;
-      store_char(c, &rx_buffer);
-    } else {
-      unsigned char c = UDR;
-    };
-  #else
-    #error UDR not defined
-  #endif
   }
-#endif
-#endif
 
 
-
-
-#if defined(USART1_RX_vect)
-  void serialEvent1() __attribute__((weak));
-  void serialEvent1() {}
-  #define serialEvent1_implemented
-  SIGNAL(USART1_RX_vect)
-  {
-    if (bit_is_clear(UCSR1A, UPE1)) {
-      unsigned char c = UDR1;
-      store_char(c, &rx_buffer1);
-    } else {
-      unsigned char c = UDR1;
-    };
-  }
-#elif defined(SIG_USART1_RECV)
-  #error SIG_USART1_RECV
-#endif
-
-
-
-
-
-#if defined(USART2_RX_vect) && defined(UDR2)
-  void serialEvent2() __attribute__((weak));
-  void serialEvent2() {}
-  #define serialEvent2_implemented
-  SIGNAL(USART2_RX_vect)
-  {
-    if (bit_is_clear(UCSR2A, UPE2)) {
-      unsigned char c = UDR2;
-      store_char(c, &rx_buffer2);
-    } else {
-      unsigned char c = UDR2;
-    };
-  }
-#elif defined(SIG_USART2_RECV)
-  #error SIG_USART2_RECV
-#endif
-
-
-
-
-
-#if defined(USART3_RX_vect) && defined(UDR3)
-  void serialEvent3() __attribute__((weak));
-  void serialEvent3() {}
-  #define serialEvent3_implemented
-  SIGNAL(USART3_RX_vect)
-  {
-    if (bit_is_clear(UCSR3A, UPE3)) {
-      unsigned char c = UDR3;
-      store_char(c, &rx_buffer3);
-    } else {
-      unsigned char c = UDR3;
-    };
-  }
-#elif defined(SIG_USART3_RECV)
-  #error SIG_USART3_RECV
-#endif
 
 
 
@@ -205,124 +61,32 @@ inline void store_char(unsigned char c, ring_buffer *buffer)
 
 void serialEventRun(void)
 {
-#ifdef serialEvent_implemented
   if (Serial.available()) serialEvent();
-#endif
-#ifdef serialEvent1_implemented
-  if (Serial1.available()) serialEvent1();
-#endif
-#ifdef serialEvent2_implemented
-  if (Serial2.available()) serialEvent2();
-#endif
-#ifdef serialEvent3_implemented
-  if (Serial3.available()) serialEvent3();
-#endif
 }
 
 
 
 
 
-#if !defined(USART0_UDRE_vect) && defined(USART1_UDRE_vect)
-// do nothing - on the 32u4 the first USART is USART1
-#else
-#if !defined(UART0_UDRE_vect) && !defined(UART_UDRE_vect) && !defined(USART0_UDRE_vect) && !defined(USART_UDRE_vect)
-  #error "Don't know what the Data Register Empty vector is called for the first UART"
-#else
-#if defined(UART0_UDRE_vect)
-ISR(UART0_UDRE_vect)
-#elif defined(UART_UDRE_vect)
-ISR(UART_UDRE_vect)
-#elif defined(USART0_UDRE_vect)
-ISR(USART0_UDRE_vect)
-#elif defined(USART_UDRE_vect)
 ISR(USART_UDRE_vect)
-#endif
 {
-  if (tx_buffer.head == tx_buffer.tail) {
+  if (tx_buffer.head == tx_buffer.tail) 
+  {
 	// Buffer empty, so disable interrupts
-#if defined(UCSR0B)
-    cbi(UCSR0B, UDRIE0);
-#else
-    cbi(UCSRB, UDRIE);
-#endif
+  cbi(UCSR0B, UDRIE0);
   }
   else {
     // There is more data in the output buffer. Send the next byte
     unsigned char c = tx_buffer.buffer[tx_buffer.tail];
     tx_buffer.tail = (tx_buffer.tail + 1) % SERIAL_BUFFER_SIZE;
 	
-  #if defined(UDR0)
     UDR0 = c;
-  #elif defined(UDR)
-    UDR = c;
-  #else
-    #error UDR not defined
-  #endif
   }
 }
-#endif
-#endif
-
-
-
-#ifdef USART1_UDRE_vect
-ISR(USART1_UDRE_vect)
-{
-  if (tx_buffer1.head == tx_buffer1.tail) {
-	// Buffer empty, so disable interrupts
-    cbi(UCSR1B, UDRIE1);
-  }
-  else {
-    // There is more data in the output buffer. Send the next byte
-    unsigned char c = tx_buffer1.buffer[tx_buffer1.tail];
-    tx_buffer1.tail = (tx_buffer1.tail + 1) % SERIAL_BUFFER_SIZE;
-	
-    UDR1 = c;
-  }
-}
-#endif
 
 
 
 
-
-#ifdef USART2_UDRE_vect
-ISR(USART2_UDRE_vect)
-{
-  if (tx_buffer2.head == tx_buffer2.tail) {
-	// Buffer empty, so disable interrupts
-    cbi(UCSR2B, UDRIE2);
-  }
-  else {
-    // There is more data in the output buffer. Send the next byte
-    unsigned char c = tx_buffer2.buffer[tx_buffer2.tail];
-    tx_buffer2.tail = (tx_buffer2.tail + 1) % SERIAL_BUFFER_SIZE;
-	
-    UDR2 = c;
-  }
-}
-#endif
-
-
-
-
-#ifdef USART3_UDRE_vect
-ISR(USART3_UDRE_vect)
-{
-  if (tx_buffer3.head == tx_buffer3.tail) {
-	// Buffer empty, so disable interrupts
-    cbi(UCSR3B, UDRIE3);
-  }
-  else {
-    // There is more data in the output buffer. Send the next byte
-    unsigned char c = tx_buffer3.buffer[tx_buffer3.tail];
-    tx_buffer3.tail = (tx_buffer3.tail + 1) % SERIAL_BUFFER_SIZE;
-	
-    UDR3 = c;
-  }
-}
-#endif
 
 
 
@@ -360,7 +124,6 @@ void SerialPort::begin(unsigned long baud)
   uint16_t baud_setting;
   bool use_u2x = true;
 
-#if F_CPU == 16000000UL
   // hardcoded exception for compatibility with the bootloader shipped
   // with the Duemilanove and previous boards and the firmware on the 8U2
   // on the Uno and Mega 2560.
@@ -368,7 +131,6 @@ void SerialPort::begin(unsigned long baud)
   {
     use_u2x = false;
   }
-#endif
 
 try_again:
 
@@ -413,14 +175,12 @@ void SerialPort::begin(unsigned long baud, byte config)
   uint8_t current_config;
   bool use_u2x = true;
 
-#if F_CPU == 16000000UL
   // hardcoded exception for compatibility with the bootloader shipped
   // with the Duemilanove and previous boards and the firmware on the 8U2
   // on the Uno and Mega 2560.
   if (baud == 57600) {
     use_u2x = false;
   }
-#endif
 
 try_again:
   
@@ -443,9 +203,6 @@ try_again:
   *_ubrrl = baud_setting;
 
   //set the data bits, parity, and stop bits
-#if defined(__AVR_ATmega8__)
-  config |= 0x80; // select UCSRC register (shared with UBRRH)
-#endif
   *_ucsrc = config;
   
   sbi(*_ucsrb, _rxen);
@@ -502,8 +259,4 @@ size_t SerialPort::write(uint8_t c)
   
   return 1;
 }
-
-
-
-#endif // whole file
 
