2f4a07bc6abb5cf05bbd004abf62c5a5a8e0f10a
U-StevesI7Laptop\steve_000 <steve_000@StevesI7Laptop.(none)>
2014-06-03 19:15:18 +0100

    - more tidying.

--- CarDisplay/SerialPort.h
+++ CarDisplay/SerialPort.h
@@ -24,45 +24,14 @@
 #define SerialPorth
 
 #include <inttypes.h>
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
 #include "Platform.h"
 #include "wiring_private.h"
-
 #include "Stream.h"
 
-
-
-
-// Define config for Serial.begin(baud, config);
-#define SERIAL5N1 0x00
-#define SERIAL6N1 0x02
-#define SERIAL7N1 0x04
-#define SERIAL8N1 0x06
-#define SERIAL5N2 0x08
-#define SERIAL6N2 0x0A
-#define SERIAL7N2 0x0C
-#define SERIAL8N2 0x0E
-#define SERIAL5E1 0x20
-#define SERIAL6E1 0x22
-#define SERIAL7E1 0x24
-#define SERIAL8E1 0x26
-#define SERIAL5E2 0x28
-#define SERIAL6E2 0x2A
-#define SERIAL7E2 0x2C
-#define SERIAL8E2 0x2E
-#define SERIAL5O1 0x30
-#define SERIAL6O1 0x32
-#define SERIAL7O1 0x34
-#define SERIAL8O1 0x36
-#define SERIAL5O2 0x38
-#define SERIAL6O2 0x3A
-#define SERIAL7O2 0x3C
-#define SERIAL8O2 0x3E
 
 
 
@@ -78,6 +47,13 @@ struct ringbuffer
   volatile unsigned int head;
   volatile unsigned int tail;
 };
+
+
+
+
+
+
+
 
 
 template <  uint32_t baud, 
@@ -100,112 +76,128 @@ class SerialPort
             ucsra((uint8_t*)_ucsra),
             ucsrb((uint8_t*)_ucsrb),
             ucsrc((uint8_t*)_ucsrc),
-            udr((uint8_t*)_udr)
+            udr((uint8_t*)_udr),
+            transmitting(false)
     {
-      uint16_t baudsetting;
-      bool useu2x = true;
+        uint16_t baudsetting;
+        bool useu2x = true;
 
-      // hardcoded exception for compatibility with the bootloader shipped
-      // with the Duemilanove and previous boards and the firmware on the 8U2
-      // on the Uno and Mega 2560.
-      if (baud == 57600) 
-      {
-        useu2x = false;
-      }
+        //
+        // hardcoded exception for compatibility with the bootloader shipped
+        // with the Duemilanove and previous boards and the firmware on the 8U2
+        // on the Uno and Mega 2560.
+        //
+        if (baud == 57600) 
+        {
+            useu2x = false;
+        }
 
-    tryagain:
+        tryagain:
 
-      if (useu2x) 
-      {
-        *ucsra = 1 << u2x;
-        baudsetting = (F_CPU / 4 / baud - 1) / 2;
-      } 
-      else 
-      {
-        *ucsra = 0;
-        baudsetting = (F_CPU / 8 / baud - 1) / 2;
-      }
-      
-      if ((baudsetting > 4095) && useu2x)
-      {
-        useu2x = false;
-        goto tryagain;
-      }
+        if (useu2x) 
+        {
+            *ucsra = 1 << u2x;
+            baudsetting = (F_CPU / 4 / baud - 1) / 2;
+        } 
+        else 
+        {
+            *ucsra = 0;
+            baudsetting = (F_CPU / 8 / baud - 1) / 2;
+        }
 
-      // assign the baudsetting, a.k.a. ubbr (USART Baud Rate Register)
-      *ubrrh = baudsetting >> 8;
-      *ubrrl = baudsetting;
+        if ((baudsetting > 4095) && useu2x)
+        {
+            useu2x = false;
+            goto tryagain;
+        }
 
-      transmitting = false;
+        //
+        // assign the baudsetting, a.k.a. ubbr (USART Baud Rate Register)
+        //
+        *ubrrh  = baudsetting >> 8;
+        *ubrrl  = baudsetting;
 
-      sbi(*ucsrb, rxen);
-      sbi(*ucsrb, txen);
-      sbi(*ucsrb, rxcie);
-      cbi(*ucsrb, udrie);
+        transmitting = false;
+
+        sbi(*ucsrb, rxen);
+        sbi(*ucsrb, txen);
+        sbi(*ucsrb, rxcie);
+        cbi(*ucsrb, udrie);
 
     }
 
 
-
-
-
-
-
+    //
+    //
+    //
     int available(void)
     {
       return (unsigned int)(SERIAL_BUFFER_SIZE + rxbuffer->head - rxbuffer->tail) % SERIAL_BUFFER_SIZE;
     }
 
 
-
+    //
+    //
+    //
     int read(void)
     {
-      // if the head isn't ahead of the tail, we don't have any characters
-      if (rxbuffer->head == rxbuffer->tail) {
-        return -1;
-      } else {
-        unsigned char c = rxbuffer->buffer[rxbuffer->tail];
-        rxbuffer->tail = (unsigned int)(rxbuffer->tail + 1) % SERIAL_BUFFER_SIZE;
-        return c;
-      }
+        //
+        // if the head isn't ahead of the tail, we don't have any characters
+        //
+        if (rxbuffer->head == rxbuffer->tail) 
+        {
+            return -1;
+        } 
+        else 
+        {
+            unsigned char c = rxbuffer->buffer[rxbuffer->tail];
+            rxbuffer->tail = (unsigned int)(rxbuffer->tail + 1) % SERIAL_BUFFER_SIZE;
+            return c;
+        }
     }
 
 
+    //
+    //
+    //
     size_t write(uint8_t c)
     {
-      int i = (txbuffer->head + 1) % SERIAL_BUFFER_SIZE;
-      
-      // If the output buffer is full, there's nothing for it other than to 
-      // wait for the interrupt handler to empty it a bit
-      // ???: return 0 here instead?
-      while (i == txbuffer->tail)
-        ;
-      
-      txbuffer->buffer[txbuffer->head] = c;
-      txbuffer->head = i;
-      
-      sbi(*ucsrb, udrie);
-      // clear the TXC bit -- "can be cleared by writing a one to its bit location"
-      transmitting = true;
-      sbi(*ucsra, TXC0);
-      
-      return 1;
+        int     i = (txbuffer->head + 1) % SERIAL_BUFFER_SIZE;
+
+        //
+        // If the output buffer is full, there's nothing for it other than to 
+        // wait for the interrupt handler to empty it a bit
+        // ???: return 0 here instead?
+        //
+        while (i == txbuffer->tail);
+
+        txbuffer->buffer[txbuffer->head] = c;
+        txbuffer->head = i;
+
+        sbi(*ucsrb, udrie);
+        // clear the TXC bit -- "can be cleared by writing a one to its bit location"
+        transmitting = true;
+        sbi(*ucsra, TXC0);
+
+        return 1;
     }
 
 
 
 private:
 
-    ringbuffer *rxbuffer;
-    ringbuffer *txbuffer;
-    bool transmitting;
-    uint8_t* ubrrh;
-    uint8_t* ubrrl;
-    uint8_t* ucsra;
-    uint8_t* ucsrb;
-    uint8_t* ucsrc;
-    uint8_t* udr;
-
+    //
+    //
+    //
+    ringbuffer* rxbuffer;
+    ringbuffer* txbuffer;
+    bool        transmitting;
+    uint8_t*    ubrrh;
+    uint8_t*    ubrrl;
+    uint8_t*    ucsra;
+    uint8_t*    ucsrb;
+    uint8_t*    ucsrc;
+    uint8_t*    udr;
 
 };
 
