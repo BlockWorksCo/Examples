f8a69515516a510534d624cb88c5dcb71b780174
Steve Tickle <Steve@ZooKoo.com>
2014-06-12 23:55:00 +0100

    - Made a demo display...

--- CarDisplay/Test.py
+++ CarDisplay/Test.py
@@ -407,32 +407,66 @@ def t1():
 def BlockWorks():
     """
     """
-    while True:
-        clear()
+    clear()
+    drawFrame()
+
+    for x in range(-20, 1):
+        clear();
+        drawText(x,0, 'Block')
         drawFrame()
+        time.sleep(0.01)
 
-        for x in range(-20, 1):
-            clear();
-            drawText(x,0, 'Block')
-            drawFrame()
-            time.sleep(0.01)
+    time.sleep(0.5)
 
-        time.sleep(0.5)
+    for x in range(40, 18, -1):
+        #clear();
+        #drawText(0,0, 'Block')
+        drawText(x,0, 'Works')
+        drawFrame()
+        time.sleep(0.01)
 
-        for x in range(40, 18, -1):
-            clear();
-            drawText(0,0, 'Block')
-            drawText(x,0, 'Works')
-            drawFrame()
-            time.sleep(0.01)
 
-        time.sleep(0.5)
+
+def BlockWorks2():
+    """
+    """
+    clear()
+    drawFrame()
+
+    for x in range(0, 20):
+        #clear();
+        drawText(-19+x,0, 'Block')
+        drawText(38-x,0, 'Works')
+        drawFrame()
+        time.sleep(0.01)
+
+
+def BlockWorks3():
+    """
+    """
+    clear()
+    drawFrame()
+
+    for x in range(0, 9):
+        clear();
+        drawText(0,-x, 'Block')
+        drawText(18,x, 'Works')
+        drawFrame()
+        time.sleep(0.01)
 
 
 #t0()
-#BlockWorks()
+
+while True:
+    BlockWorks2()
+    time.sleep(0.5)
+    BlockWorks()
+    time.sleep(0.5)
+    BlockWorks3()
+    time.sleep(0.5)
+
 #VertTest()
-VertClock()
+#VertClock()
 #ser.close() 
 
 
