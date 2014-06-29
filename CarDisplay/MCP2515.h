891a1262091d935a682e99705650969668f1e954
Steve Tickle <Steve@ZooKoo.com>
2014-06-29 23:19:08 +0100

    - Added panic. - Mode work on the MCP2515.

--- CarDisplay/MCP2515.h
+++ CarDisplay/MCP2515.h
@@ -3,6 +3,8 @@
 #ifndef __MCP2515_H__
 #define __MCP2515_H__
 
+#include "Platform.h"
+
 
 
 //
@@ -26,15 +28,32 @@ public:
     //
     // init can                  
     //
-    int8_t begin(int8_t speedset)
+    uint8_t begin(uint8_t speedset)
     {
         return 0;
     }                              
 
+
+    void SetCANCTRL_Mode(const uint8_t newmode)
+    {
+        uint8_t i;
+
+        ModifyRegister(MCP_CANCTRL, MODE_MASK, newmode);
+
+        i = ReadRegister(MCP_CANCTRL);
+        i &= MODE_MASK;
+
+        if ( i != newmode ) 
+        {
+            PANIC(1);
+        }
+    }
+
+
     //
     // init Masks                 
     //
-    int8_t init_Mask(int8_t num, int8_t ext, int32_t ulData)
+    uint8_t init_Mask(uint8_t num, uint8_t ext, uint8_t ulData)
     {
         return 0;
 
@@ -43,7 +62,7 @@ public:
     //
     // init filters               
     //
-    int8_t init_Filt(int8_t num, int8_t ext, int32_t ulData)
+    uint8_t init_Filt(uint8_t num, uint8_t ext, uint32_t ulData)
     {
 
         return 0;
@@ -52,7 +71,7 @@ public:
     //
     // send buf                    
     //
-    int8_t sendMsgBuf(int32_t id, int8_t ext, int8_t len, int8_t *buf)
+    uint8_t sendMsgBuf(uint32_t id, uint8_t ext, uint8_t len, uint8_t *buf)
     {
 
         return 0;
@@ -61,7 +80,7 @@ public:
     //
     // read buf                    
     //
-    int8_t readMsgBuf(int8_t *len, int8_t *buf)
+    uint8_t readMsgBuf(uint8_t *len, uint8_t *buf)
     {
 
         return 0;
@@ -70,7 +89,7 @@ public:
     //
     // if something received       
     //
-    int8_t checkReceive(void)
+    uint8_t checkReceive(void)
     {
 
         return 0;
@@ -79,7 +98,7 @@ public:
     //
     // if something error       
     //
-    int8_t checkError(void)
+    uint8_t checkError(void)
     {
 
         return 0;
@@ -88,7 +107,7 @@ public:
     //
     // get can id when receive
     //
-    int32_t getCanId(void)
+    uint32_t getCanId(void)
     {
 
         return 0;
@@ -130,7 +149,7 @@ private:
     //
     //
     //
-    int8_t readRegister(const uint8_t address)                                                                     
+    uint8_t readRegister(const uint8_t address)                                                                     
     {
         uint8_t ret;
 
@@ -196,6 +215,18 @@ private:
 
     //
     //
+    //
+    void Reset(void)                                      
+    {
+        chipSelect.Set();
+        spi.readwrite(MCP_RESET);
+        chipSelect.Clear();
+        delay(10);
+    }
+
+
+    //
+    //
     //                                                                                                                                                                                                                ,vvvvvvvvvvvvvvlg,mm   
     typedef enum 
     {
@@ -254,6 +285,91 @@ private:
     } Instruction;
 
 
+    //
+    // CAN control values.
+    //
+    typedef enum
+    {
+        MODE_NORMAL     = 0x00,
+        MODE_SLEEP      = 0x20,
+        MODE_LOOPBACK   = 0x40,
+        MODE_LISTENONLY = 0x60,
+        MODE_CONFIG     = 0x80,
+        MODE_POWERUP    = 0xE0,
+        MODE_MASK       = 0xE0,
+        ABORT_TX        = 0x10,
+        MODE_ONESHOT    = 0x08,
+        CLKOUT_ENABLE   = 0x04,
+        CLKOUT_DISABLE  = 0x00,
+        CLKOUT_PS1      = 0x00,
+        CLKOUT_PS2      = 0x01,
+        CLKOUT_PS4      = 0x02,
+        CLKOUT_PS8      = 0x03,
+    };
+
+
+    //
+    //
+    //
+    typedef enum 
+    {
+        MCP_RXF0SIDH    = 0x00,
+        MCP_RXF0SIDL    = 0x01,
+        MCP_RXF0EID8    = 0x02,
+        MCP_RXF0EID0    = 0x03,
+        MCP_RXF1SIDH    = 0x04,
+        MCP_RXF1SIDL    = 0x05,
+        MCP_RXF1EID8    = 0x06,
+        MCP_RXF1EID0    = 0x07,
+        MCP_RXF2SIDH    = 0x08,
+        MCP_RXF2SIDL    = 0x09,
+        MCP_RXF2EID8    = 0x0A,
+        MCP_RXF2EID0    = 0x0B,
+        MCP_CANSTAT     = 0x0E,
+        MCP_CANCTRL     = 0x0F,
+        MCP_RXF3SIDH    = 0x10,
+        MCP_RXF3SIDL    = 0x11,
+        MCP_RXF3EID8    = 0x12,
+        MCP_RXF3EID0    = 0x13,
+        MCP_RXF4SIDH    = 0x14,
+        MCP_RXF4SIDL    = 0x15,
+        MCP_RXF4EID8    = 0x16,
+        MCP_RXF4EID0    = 0x17,
+        MCP_RXF5SIDH    = 0x18,
+        MCP_RXF5SIDL    = 0x19,
+        MCP_RXF5EID8    = 0x1A,
+        MCP_RXF5EID0    = 0x1B,
+        MCP_TEC         = 0x1C,
+        MCP_REC         = 0x1D,
+        MCP_RXM0SIDH    = 0x20,
+        MCP_RXM0SIDL    = 0x21,
+        MCP_RXM0EID8    = 0x22,
+        MCP_RXM0EID0    = 0x23,
+        MCP_RXM1SIDH    = 0x24,
+        MCP_RXM1SIDL    = 0x25,
+        MCP_RXM1EID8    = 0x26,
+        MCP_RXM1EID0    = 0x27,
+        MCP_CNF3        = 0x28,
+        MCP_CNF2        = 0x29,
+        MCP_CNF1        = 0x2A,
+        MCP_CANINTE     = 0x2B,
+        MCP_CANINTF     = 0x2C,
+        MCP_EFLG        = 0x2D,
+        MCP_TXB0CTRL    = 0x30,
+        MCP_TXB1CTRL    = 0x40,
+        MCP_TXB2CTRL    = 0x50,
+        MCP_RXB0CTRL    = 0x60,
+        MCP_RXB0SIDH    = 0x61,
+        MCP_RXB1CTRL    = 0x70,
+        MCP_RXB1SIDH    = 0x71,
+        MCP_TX_INT      = 0x1C,                                    // Enable all transmit interrup ts
+        MCP_TX01_INT    = 0x0C,                                    // Enable TXB0 and TXB1 interru pts
+        MCP_RX_INT      = 0x03,                                    // Enable receive interrupts
+        MCP_NO_INT      = 0x00,                                    // Disable all interrupts
+        MCP_TX01_MASK   = 0x14,
+        MCP_TX_MASK     = 0x54,
+
+    } Registers;    
 
     SPIType&        spi;
     ChipSelectType  chipSelect;
