4566a3ae23a9f9e62bf7a045c9615c7a4c6bcb8c
Steve Tickle <Steve@ZooKoo.com>
2014-07-14 22:41:11 +0100

    - Trying to get a basic UART output working.

--- DisplayDaemon/AppMain.cpp
+++ DisplayDaemon/AppMain.cpp
@@ -12,65 +12,12 @@
 //
 //
 //
-TimingType 									        timing;
-EventEngineType								        eventEngine(timing);
 
-RxQueueType                                         rxQueue;
-TxQueueType                                         txQueue;
-DataOutputType                                      dataOutput;
-LoadOutputType                                      loadOutput;
-ClockOutputType                                     clockOutput;
-DisplayType                                         display(dataOutput, loadOutput, clockOutput);
+RxQueueType        rxQueue;
+TxQueueType        txQueue;
 
-SPIRxQueueType                                      spiRxQueue;
-SPITxQueueType                                      spiTxQueue;
-SPIType                                             spi(spiRxQueue, spiTxQueue);
-MCP2515ChipSelectType                               mcp2515ChipSelect;
-MCP2515Type                                         mcp2515(spi, mcp2515ChipSelect);
+UARTType           uart0("/dev/ttyS0", rxQueue,txQueue);
 
-extern MessageHandlingPair::ByteReceivedEventType   uartByteAvailableHandler;
-MessageHandlingPair::UARTType                       uart0(rxQueue,txQueue, eventEngine, uartByteAvailableHandler);
-
-extern MessageHandlingPair::handlerType             carDisplay;
-MessageHandlingPair::protocolType                   protocol(uart0, display, rxQueue, txQueue, carDisplay);
-MessageHandlingPair::handlerType                    carDisplay(display, protocol);
-
-
- MessageHandlingPair::ByteReceivedEventType 		uartByteAvailableHandler(protocol, &MessageHandlingPair::protocolType::Process);
-
-
-
-
-ISR(USART_RX_vect)
-{
-    uart0.RxISR();
-}
-
-
-ISR(USART_UDRE_vect)
-{
-   uart0.TxISR();
-}
-
-
-ISR(SPI_STC_vect)
-{
-    spi.TransferCompleteISR();
-}
-
-#if 0
-ISR(TIMER0_OVF_vect)
-{
-    timing.TickISR();
-}
-#endif
-
-void DebugOut(uint8_t ch)
-{
-    bool blaa;
-    txQueue.Put(ch, blaa);
-    uart0.Process();
-}
 
 
 
@@ -85,15 +32,12 @@ void DebugOut(uint8_t ch)
 //
 extern "C" void AppMain()
 {
-    uart0.start();
-
-    carDisplay.Initialise();
-
+    bool    elementDroppedFlag  = false;
+    
     while(true)
     {
-        sei();
-
-        eventEngine();
+        txQueue.Put('a', elementDroppedFlag)
+        uart0.ProcessQueue();
     }
 }
 
--- DisplayDaemon/Linux/AppConfiguration.h
+++ DisplayDaemon/Linux/AppConfiguration.h
@@ -7,8 +7,6 @@
 
 
 #include "Platform.h"
-#include "Display.h"
-#include "CarDisplay.h"
 #include "UART.h"
 #include "SimpleBinaryProtocol.h"
 #include "Queue.h"
@@ -24,23 +22,11 @@
 // a substitute mechanism (inheritance).
 //
 
-
-typedef Display<2,
-            3,
-            4,
-            5,
-            5 >     DisplayType;
-
 typedef Queue<uint8_t, 8, uint8_t>     RxQueueType;
 typedef Queue<uint8_t, 8, uint8_t>     TxQueueType;
 
-typedef UART<3,
-             RxQueueType, TxQueueType,
-             19200>     UARTType;
-
-typedef SimpleBinaryProtocol<UARTType, DisplayType, RxQueueType, TxQueueType>   ProtocolType;
-
-typedef CarDisplay<DisplayType, ProtocolType>   CarDisplayType;
+typedef UART<RxQueueType, TxQueueType,
+             115200>                    UARTType;
 
 
 #endif
