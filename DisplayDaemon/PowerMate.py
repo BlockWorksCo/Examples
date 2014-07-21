c16b28efa938fb22f912f62f2125653262adbdc2
Steve Tickle <Steve@ZooKoo.com>
2014-07-21 21:50:07 +0100

    PowerMate.py seems to work.

--- DisplayDaemon/PowerMate.py
+++ DisplayDaemon/PowerMate.py
@@ -3,7 +3,7 @@ import struct
 import time
 import sys
 
-infile_path = "/dev/input/event" + (sys.argv[1] if len(sys.argv) > 1 else "0")
+infile_path = "/dev/input/by-id/usb-Griffin_Technology__Inc._Griffin_PowerMate-event-if00"
 
 #long int, long int, unsigned short, unsigned short, unsigned int
 FORMAT = 'llHHI'
@@ -17,12 +17,24 @@ event = in_file.read(EVENT_SIZE)
 while event:
     (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
 
-    if type != 0 or code != 0 or value != 0:
-        print("Event type %u, code %u, value: %u at %d, %d" % \
-            (type, code, value, tv_sec, tv_usec))
-    else:
-        # Events with code, type and value == 0 are "separator" events
-        print("===========================================")
+    if type == 1 and value == 1:
+    	print('down')
+
+    if type == 1 and value == 0:
+    	print('up')
+
+    if type == 2 and value == 1:
+    	print('right')
+
+    if type == 2 and value == 4294967295:
+    	print('left')
+
+    #if type != 0 or code != 0 or value != 0:
+    #    print("Event type %u, code %u, value: %u at %d, %d" % \
+    #        (type, code, value, tv_sec, tv_usec))
+    #else:
+    #    # Events with code, type and value == 0 are "separator" events
+    #    print("===========================================")
 
     event = in_file.read(EVENT_SIZE)
 
