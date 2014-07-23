411a7f614756c438e035f8e3a77425947230bffb
Steve Tickle <Steve@ZooKoo.com>
2014-07-23 14:15:06 +0100

    Added a trivial HelloWorld project to test the toolchain.

---
+++ HelloWorld/AppMain.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+//
+// Entry point for the application.
+// 
+// Note1: The main application object is created static within main instead
+// of being global in order to control initialisation-time.
+// Note2: 'Run' methods are one-shot. The loop is implicit to allow for transition
+// between threaded and non-threaded mechanisms.
+//
+extern "C" void AppMain()
+{
+    while(true)
+    {
+    }
+}
+
+
+
+
