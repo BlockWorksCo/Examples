a322da8322778246f86c18e2e08ba2f7c829b46f
Steve Tickle <Steve@ZooKoo.com>
2014-08-19 07:19:10 +0100

    - Added tun0 creation.

--- miniweb/tun_dev.h
+++ miniweb/tun_dev.h
@@ -35,6 +35,10 @@
 #include <linux/if_tun.h>
 #include <netinet/in.h>
 
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+
 
 
 
@@ -59,9 +63,16 @@ public:
         webServer(_webServer),
         dropFlag(0)
     {
-        /*  int val;*/
-        fd = open("/dev/tun0", O_RDWR);
 
+        char tun_name[IFNAMSIZ];
+
+        strcpy(tun_name, "tun1");
+        fd = tun_alloc(tun_name, IFF_TUN);  /* tun interface */
+
+        /*  int val;*/
+#if 0        
+        fd = open("/dev/tun0", O_RDWR);
+#endif
         if(fd == -1)
         {
             perror("tun_dev: dev_init: open");
@@ -85,6 +96,53 @@ public:
         outptr = 0;
     }
 
+
+    int tun_alloc(char *dev, int flags) {
+
+      struct ifreq ifr;
+      int fd, err;
+      const char *clonedev = "/dev/net/tun";
+
+      /* Arguments taken by the function:
+       *
+       * char *dev: the name of an interface (or '\0'). MUST have enough
+       *   space to hold the interface name if '\0' is passed
+       * int flags: interface flags (eg, IFF_TUN etc.)
+       */
+
+       /* open the clone device */
+       if( (fd = open(clonedev, O_RDWR)) < 0 ) {
+         return fd;
+       }
+
+       /* preparation of the struct ifr, of type "struct ifreq" */
+       memset(&ifr, 0, sizeof(ifr));
+
+       ifr.ifr_flags = flags;   /* IFF_TUN or IFF_TAP, plus maybe IFF_NO_PI */
+
+       if (*dev) {
+         /* if a device name was specified, put it in the structure; otherwise,
+          * the kernel will try to allocate the "next" device of the
+          * specified type */
+         strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+       }
+
+       /* try to create the device */
+       if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ) {
+         close(fd);
+         return err;
+       }
+
+      /* if the operation was successful, write back the name of the
+       * interface to the variable "dev", so the caller can know
+       * it. Note that the caller MUST reserve space in *dev (see calling
+       * code below) */
+      strcpy(dev, ifr.ifr_name);
+
+      /* this is the special file descriptor that the caller will use to talk
+       * with the virtual interface */
+      return fd;
+    }
 
     //
     //
