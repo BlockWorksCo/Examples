9370801e4731c28e6403fe4561e217db767e1c1c
Steve Tickle <Steve@ZooKoo.com>
2014-09-18 14:54:13 +0100

    - Added a Tests.py to allow nose to run them.

---
+++ IPStack/Tests/Tests.py
@@ -0,0 +1,37 @@
+
+
+from nose import with_setup
+import sys
+import subprocess
+import shlex
+import os
+
+
+
+def setup_module():
+    """
+    """
+    pass
+
+
+
+def teardown_module():
+    """
+    """
+    pass
+
+
+
+
+def TestChecksums():
+    """
+    """
+    print('Checksum Tests')
+    assert False
+
+
+
+
+
+
+
