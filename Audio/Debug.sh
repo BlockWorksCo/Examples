72e27be3e3ca265eeed2a07aa4ae3fed9d802e55
Steve Tickle <Steve@ZooKoo.com>
2014-10-23 23:50:02 +0100

    -

--- Audio/Debug.sh
+++ Audio/Debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-BuildVMssh -t "cd /HostRoot/home/steve/Projects/Audio && arm-none-eabi-gdb Output/Main -ex 'tar rem :4242' "
+BuildVMssh -t "cd /HostRoot/home/steve/BlockWorks/Examples/Audio && arm-none-eabi-gdb Output/Main -ex 'tar rem :4242' "
 
