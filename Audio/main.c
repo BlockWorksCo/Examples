8589aa72f7c611fc10225f2b0663e290684b8ce9
Steve Tickle <Steve@ZooKoo.com>
2014-10-25 23:55:18 +0100

    - Now enumerates... clock config was incorrect.

--- Audio/main.c
+++ Audio/main.c
@@ -80,29 +80,13 @@ extern "C" int main(void)
 
     GPIO_InitTypeDef  GPIO_InitStruct;
 
-
-    /* Configure DM DP Pins */
-    GPIO_InitStruct.Pin       = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-    GPIO_InitStruct.Mode      = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
-    GPIO_InitStruct.Mode      = GPIO_MODE_OUTPUT_PP;
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+    /* Configure the GPIO_LED pin */
     __GPIOD_CLK_ENABLE();
-  
-
-
-  /* Configure the GPIO_LED pin */
-  GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
-  
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-  
-  __GPIOD_CLK_ENABLE();
-  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET); 
-  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET); 
+    GPIO_InitStruct.Pin     = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+    GPIO_InitStruct.Mode    = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull    = GPIO_PULLUP;
+    GPIO_InitStruct.Speed   = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
     volatile GPIO_TypeDef*  gpio = GPIOD;
     gpio->ODR = 0xff;
@@ -143,43 +127,36 @@ extern "C" int main(void)
   */
 static void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInit;
-    RCC_ClkInitTypeDef RCC_ClkInit;
-    //RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
 
     /* Enable Power Control clock */
     __PWR_CLK_ENABLE();
 
-    /* The voltage scaling allows optimizing the power consumption when the device is
-       clocked below the maximum system frequency, to update the voltage scaling value
-       regarding system frequency refer to product datasheet.  */
+    /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /* Configure RCC Oscillators: All parameters can be changed according to user’s needs */
-    RCC_OscInit.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInit.HSEState = RCC_HSE_ON;
-    RCC_OscInit.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInit.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInit.PLL.PLLM = 25;
-    RCC_OscInit.PLL.PLLN = 336;
-    RCC_OscInit.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInit.PLL.PLLQ = 7;
-    HAL_RCC_OscConfig (&RCC_OscInit);
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    /* RCC Clocks: All parameters can be changed according to user’s needs */
-    RCC_ClkInit.ClockType = RCC_CLOCKTYPE_SYSCLK |RCC_CLOCKTYPE_HCLK |RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInit.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInit.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInit.APB1CLKDivider = RCC_HCLK_DIV4;
-    RCC_ClkInit.APB2CLKDivider = RCC_HCLK_DIV2;
-    HAL_RCC_ClockConfig (&RCC_ClkInit, FLASH_LATENCY_5);
-#if 0
-    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SAI_PLLI2S;
-    PeriphClkInitStruct.PLLI2S.PLLI2SN = 429;
-    PeriphClkInitStruct.PLLI2S.PLLI2SQ = 2;
-    PeriphClkInitStruct.PLLI2SDivQ = 19;
-    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
-#endif
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 }
 
 
